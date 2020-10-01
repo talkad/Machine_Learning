a30833292b2997d1a90e491d13524bf8f9ce6ef3
talkad <talkad@post.bgu.ac.il>
2020-10-01 17:46:12 +0300

    impelemented the model with convolutional neural network

--- Computer_Vision/app.py
+++ Computer_Vision/app.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk  # for graphical interface
 from tkinter import simpledialog
+
 import PIL.Image
 import PIL.ImageTk
 from Computer_Vision import camera
@@ -32,61 +33,72 @@ class App:
         self.window.mainloop()
 
     def init_gui(self):
-
         self.classname_one = simpledialog.askstring("Classname One", "Enter the name of the first class:",
                                                     parent=self.window)
         self.classname_two = simpledialog.askstring("Classname Two", "Enter the name of the second class:",
                                                     parent=self.window)
 
         self.head_label = tk.Label(self.window, text="Computer Vision- Camera Classifier:", font=("Helvetica", 18))
-        self.head_label.pack(anchor=tk.CENTER, expand=True)
+        self.head_label.grid(row=0, column=1)
 
         self.class_label = tk.Label(self.window, text="-- blank --", font=("Helvetica", 16))
-        self.class_label.pack(anchor=tk.CENTER, expand=True)
+        self.class_label.grid(row=1, column=1)
 
         self.canvas = tk.Canvas(self.window, width=self.camera.width, height=self.camera.height)
-        self.canvas.pack()
+        self.canvas.grid(row=2, column=1)
 
-        self.btn_toggleauto = tk.Button(self.window, text="Toggle Auto Prediction", width=50, font=("Helvetica", 12),
+        self.error_label = tk.Label(self.window, text="", font=("Helvetica", 16), foreground='red')
+        self.error_label.grid(row=3, column=1)
+
+        self.btn_class_one = tk.Button(self.window, text=f'{self.classname_one} - quantity: 0', width=30,
+                                       font=("Helvetica", 12), command=lambda: self.save_for_class(1))
+        self.btn_class_one.grid(row=5, column=0)
+
+        self.btn_class_two = tk.Button(self.window, text=f'{self.classname_two} - quantity: 0', width=30,
+                                       font=("Helvetica", 12), command=lambda: self.save_for_class(2))
+        self.btn_class_two.grid(row=5, column=5)
+
+        self.btn_toggleauto = tk.Button(self.window, text="Toggle Auto Prediction", width=60, font=("Helvetica", 12),
                                         command=self.auto_predict_toggle)
-        self.btn_toggleauto.pack(anchor=tk.CENTER, expand=True)
+        self.btn_toggleauto.grid(row=4, column=1)
 
-        self.btn_class_one = tk.Button(self.window, text=self.classname_one, width=50, font=("Helvetica", 12),
-                                       command=lambda: self.save_for_class(1))
-        self.btn_class_one.pack(anchor=tk.CENTER, expand=True)
+        self.btn_train = tk.Button(self.window, text="Train Model", width=60, font=("Helvetica", 12),
+                                   command=lambda: self.model.train_model() if self.counters[0] >= 50 and self.counters[
+                                       1] >= 50 else False)
+        self.btn_train.grid(row=5, column=1)
 
-        self.btn_class_two = tk.Button(self.window, text=self.classname_two, width=50, font=("Helvetica", 12),
-                                       command=lambda: self.save_for_class(2))
-        self.btn_class_two.pack(anchor=tk.CENTER, expand=True)
-
-        self.btn_train = tk.Button(self.window, text="Train Model", width=50, font=("Helvetica", 12),
-                                   command=lambda: self.model.train_model(self.counters))
-        self.btn_train.pack(anchor=tk.CENTER, expand=True)
-
-        self.btn_predict = tk.Button(self.window, text="Predcit", width=50, font=("Helvetica", 12), command=self.predict)
-        self.btn_predict.pack(anchor=tk.CENTER, expand=True)
+        self.btn_predict = tk.Button(self.window, text="Predcit", width=60, font=("Helvetica", 12),
+                                     command=self.predict)
+        self.btn_predict.grid(row=6, column=1)
 
     def auto_predict_toggle(self):
         self.auto_predict = not self.auto_predict
 
     def save_for_class(self, class_num):
         ret, frame = self.camera.get_frame()
-        if not os.path.exists("1"):
-            os.mkdir("1")
-        if not os.path.exists("2"):
-            os.mkdir("2")
+        if not os.path.exists("img"):
+            os.mkdir("img")
+        if not os.path.exists("img/1"):
+            os.mkdir("img/1")
+        if not os.path.exists("img/2"):
+            os.mkdir("img/2")
 
         # pre-processing. reduce the amount of data in order to reduce the training time.
-        cv.imwrite(f'{class_num}/frame{self.counters[class_num - 1]}.jpg', cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
-        img = PIL.Image.open(f'{class_num}/frame{self.counters[class_num - 1]}.jpg')
-        img.thumbnail((150, 150), PIL.Image.ANTIALIAS)  # Pooling
-        img.save(f'{class_num}/frame{self.counters[class_num - 1]}.jpg')
+        img_path = f'img/{class_num}/frame{self.counters[class_num - 1]}.jpg'
+        # cv.imwrite(img_path, cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
+        cv.imwrite(img_path, frame)
+        img = PIL.Image.open(img_path)
+        img.thumbnail((150, 150), PIL.Image.ANTIALIAS)
+        img.save(img_path)
 
         self.counters[class_num - 1] += 1
+        # update the quantity
+        self.btn_class_one.config(text=f'{self.classname_one} - quantity:{self.counters[0]}')
+        self.btn_class_two.config(text=f'{self.classname_two} - quantity:{self.counters[1]}')
 
     def update(self):
         if self.auto_predict:
-            print(self.predict())
+            self.predict()
 
         ret, frame = self.camera.get_frame()
 
@@ -97,12 +109,23 @@ class App:
         self.window.after(self.delay, self.update)
 
     def predict(self):
-        frame = self.camera.get_frame()
-        prediction = self.model.predict(frame)
+        if self.counters[0] >= 50 and self.counters[1] >= 50:
+            frame = self.camera.get_frame()
+            prediction = self.model.predict(frame)
 
-        if prediction == 1:
+            self.error_label.config(text='')
+            self.show_prediction(prediction)
+        else:
+            self.error_label.config(text='Error: You need to provide at least 50 images of each class')
+
+    def show_prediction(self, prediction):
+        if prediction <= 0.4:
             self.class_label.config(text=self.classname_one)
             return self.classname_one
-        if prediction == 2:
+        elif prediction >= 0.6:
             self.class_label.config(text=self.classname_two)
             return self.classname_two
+        else:
+            self.class_label.config(text='I have no idea')
+            return 'I have no idea'
+
--- Computer_Vision/model.py
+++ Computer_Vision/model.py
@@ -1,74 +1,103 @@
-import numpy as np
 import cv2 as cv
 import PIL
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Reshape
-from keras.losses import categorical_crossentropy
-from keras.utils import np_utils
-import tensorflow as tf
-
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
+from keras.preprocessing.image import ImageDataGenerator
 
 # the model implemented by Convolution Neural Networks and Image Recognition
 # (MPLClassifier or SVM should be enough for this project)
+from setuptools import glob
+
+
 class Model:
 
     def __init__(self):
         # building a linear stack of layers with the sequential model
         self.model = Sequential()
         self.model.add(Conv2D(32, kernel_size=(3, 3),
-                                activation='relu',
-                                input_shape=(150, 150, 1)))  # the first input is the number of images
-        # each one of them is 150*150 pixels in Gray-Scale (one channel)
+                              activation='relu',
+                              input_shape=(150, 113, 3)))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        # convolutional layers
-        self.model.add(MaxPooling2D((2, 2)))
-        self.model.add(Dropout(0.25))
-        self.model.add(Conv2D(64, (4, 4), activation='relu'))
-        self.model.add(MaxPooling2D((2, 2)))
-        self.model.add(Dropout(0.25))
-        self.model.add(Conv2D(64, (4, 4), activation='relu'))
+        self.model.add(Conv2D(32, 3, 3, activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        self.model.add(Flatten())
-        self.model.add(Dense(64, activation='sigmoid'))
-        self.model.add(Dropout(0.4))
-        self.model.add(Dense(2, activation='softmax'))  # number of classes at the end layer
+        self.model.add(Conv2D(64, 3, 3, activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    def train_model(self, counters):
-        X_train = np.array([])
-        y_train = np.array([])
+        self.model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 
-        for i in range(0, counters[0]):
-            img = cv.imread(f'1/frame{i}.jpg')[:, :, 0]
-            img = img.reshape(16950)
-            X_train = np.append(X_train, [img])
-            y_train = np.append(y_train, 1)
+        # create neural network of three layers: 2 of them with 16 nodes and the output layer with one node (0/1)
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(1, activation='sigmoid'))
 
-        for i in range(0, counters[1]):
-            img = cv.imread(f'2/frame{i}.jpg')[:, :, 0]
-            img = img.reshape(16950)
-            X_train = np.append(X_train, [img])
-            y_train = np.append(y_train, 2)
-
-        X_train = X_train.reshape(counters[0] + counters[1], 16950)
-        self.model.compile(loss=categorical_crossentropy,
-                           optimizer=tf.optimizers.SGD(lr=0.01),
+        self.model.compile(loss='binary_crossentropy',
+                           optimizer='rmsprop',
                            metrics=['accuracy'])
 
-        self.model.fit(X_train, y_train,
-                       batch_size=20,
-                       epochs=10,
-                       verbose=1)
+        self.model.summary()
+
+    def train_model(self):
+        # first try
+        # X_train = np.array([])
+        # y_train = np.array([])
+        #
+        # filenames = [img for img in glob.glob("img/*.jpg")]
+        # shuffle(filenames)
+        #
+        # for filename in filenames:
+        #     match = re.match(r'frame(\d+)class(\d+)', filename[4: -4])
+        #     frame = int(match.group(1))
+        #     class_num = int(match.group(2))
+        #
+        #     img = cv.imread(f'img/frame{frame}class{class_num}.jpg')
+        #     img = img.reshape(150, 113, 3)
+        #     X_train = np.append(X_train, [img])
+        #     y_train = np.append(y_train, [class_num])
+        #
+        # X_train = X_train.reshape((counters[0] + counters[1], 150, 113, 3))
+        # X_train = X_train / 255.0
+        #
+        # self.model.fit(X_train, y_train,
+        #                batch_size=64,
+        #                epochs=50,
+        #                verbose=0)
+
+        # this is the augmentation configuration we will use for training
+        train_datagen = ImageDataGenerator(
+            rescale=1. / 255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+
+        train_generator = train_datagen.flow_from_directory(
+            'img',
+            target_size=(150, 113),
+            batch_size=16,
+            class_mode='binary')
+
+        self.model.fit(
+            train_generator,
+            # steps_per_epoch=100,
+            epochs=50)
+
+        self.model.save_weights('CNN.h5')
         print("Model successfully trained!")
 
     def predict(self, frame):
         frame = frame[1]
-        cv.imwrite("frame.jpg", cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
+        # cv.imwrite("frame.jpg", cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
+        cv.imwrite("frame.jpg", frame)
         img = PIL.Image.open("frame.jpg")
         img.thumbnail((150, 150), PIL.Image.ANTIALIAS)
         img.save("frame.jpg")
 
-        img = cv.imread('frame.jpg')[:, :, 0]
-        img = img.reshape(16950)
+        img = cv.imread('frame.jpg')
+        img = img.reshape(1, 150, 113, 3)
+
+        img = img / 255.0
+
         prediction = self.model.predict([img])
 
-        return prediction[0]
+        return prediction[0][0]
